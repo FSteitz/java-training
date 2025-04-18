049cbc25b9e5bbbf10f963b6a7d8f680fb5462e4
Florian Steitz <florian@fsteitz.com>
2025-04-18 22:38:12 +0200

    Add main method to SpringBootApplication
    It includes a simple implementation to bootstrap Spring Boot with a single RestController

--- java-training-springboot/src/main/java/com/github/fsteitz/training/springboot/SpringBootApplication.java
+++ java-training-springboot/src/main/java/com/github/fsteitz/training/springboot/SpringBootApplication.java
@@ -1,4 +1,22 @@
 package com.github.fsteitz.training.springboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
 public class SpringBootApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(SpringBootApplication.class, args);
+   }
+
+   @RestController
+   public static class GreetingController {
+
+      @GetMapping("/greet")
+      public String greet() {
+         return "Welcome to the training!";
+      }
+   }
 }
