06f149ca5436aacb403d4841eef10551088e9a50
Florian Steitz <florian@fsteitz.com>
2025-04-18 19:45:44 +0200

    Add main method to CliApplication

--- java-training-cli/src/main/java/com/github/fsteitz/training/cli/CliApplication.java
+++ java-training-cli/src/main/java/com/github/fsteitz/training/cli/CliApplication.java
@@ -1,4 +1,8 @@
 package com.github.fsteitz.training.cli;
 
 public class CliApplication {
+
+   public static void main(String[] args) {
+      System.out.println("Welcome to the training!");
+   }
 }
