2351c3dbb70eda184b9ff11684e30fc3fe941224
Florian Steitz <florian@fsteitz.com>
2025-04-18 22:39:19 +0200

    Replace FQN with import

--- java-training-springboot/src/main/java/com/github/fsteitz/training/springboot/SpringBootApp.java
+++ java-training-springboot/src/main/java/com/github/fsteitz/training/springboot/SpringBootApp.java
@@ -1,10 +1,11 @@
 package com.github.fsteitz.training.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.boot.autoconfigure.SpringBootApplication
+@SpringBootApplication
 public class SpringBootApp {
 
    public static void main(String[] args) {
