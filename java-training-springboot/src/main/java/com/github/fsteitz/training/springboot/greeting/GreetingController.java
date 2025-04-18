8285e9e57e7be106ab63236bbe88421c17eda008
Florian Steitz <florian@fsteitz.com>
2025-04-18 23:01:30 +0200

    Extract GreetingController

--- java-training-springboot/src/main/java/com/github/fsteitz/training/springboot/greeting/GreetingController.java
+++ java-training-springboot/src/main/java/com/github/fsteitz/training/springboot/greeting/GreetingController.java
@@ -13,16 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.fsteitz.training.springboot;
+package com.github.fsteitz.training.springboot.greeting;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.github.fsteitz.training.common.JsonUtil;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class SpringBootApp {
+@RestController
+public class GreetingController {
 
-   public static void main(String[] args) {
-      SpringApplication.run(SpringBootApp.class, args);
+   @GetMapping(value = "/greet", produces = MediaType.APPLICATION_JSON_VALUE)
+   public String greet() {
+      return JsonUtil.toJson(new Greeting("Welcome to the training!"));
    }
-
 }
