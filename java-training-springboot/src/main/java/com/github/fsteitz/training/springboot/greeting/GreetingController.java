e4bf1d847ec8a1f04fecc94656170175c62e6f32
Florian Steitz <florian@fsteitz.com>
2025-04-19 18:22:35 +0200

    Remove usage of JsonUtil in GreetingController

--- java-training-springboot/src/main/java/com/github/fsteitz/training/springboot/greeting/GreetingController.java
+++ java-training-springboot/src/main/java/com/github/fsteitz/training/springboot/greeting/GreetingController.java
@@ -15,7 +15,6 @@
  */
 package com.github.fsteitz.training.springboot.greeting;
 
-import com.github.fsteitz.training.common.JsonUtil;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
    @GetMapping(value = "/training", produces = MediaType.APPLICATION_JSON_VALUE)
-   public String greet() {
-      return JsonUtil.toJson(new Greeting("Welcome to the sword training!"));
+   public Greeting greet() {
+      return new Greeting("Welcome to the sword training!");
    }
 }
